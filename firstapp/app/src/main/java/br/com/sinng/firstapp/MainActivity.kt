fe4bc9437dd29f8b8787c72e2d9d422ed6e4331b
Ingrid Silva <sinngtec@gmail.com>
2024-06-11 20:28:07 -0300

    add final implementation

--- firstapp/app/src/main/java/br/com/sinng/firstapp/MainActivity.kt
+++ firstapp/app/src/main/java/br/com/sinng/firstapp/MainActivity.kt
@@ -2,10 +2,20 @@ package br.com.sinng.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.sinng.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonName.setOnClickListener {
+            val name = binding.editName.text.toString()
+//            binding.textResult.text = "Olá ${name}"
+//            binding.textResult.text = "Olá " + name
+            binding.textResult.setText("Olá ${name}")
+        }
     }
 }
